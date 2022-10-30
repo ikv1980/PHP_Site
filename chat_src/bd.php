8e7496106611e255fc447f43120b4f071089f594
Konstantin Ivanov <ikv1980@gmail.com>
2022-10-30 22:55:56 +0300

    Delete folder CHAT_SRC

--- chat_src/bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Подключаемся к mysql серверу
-//имя - localhost
-//юзер - root
-//пароль - нету
-$mysql_connect=mysql_connect("localhost","root","");
-//Выбираем базу данных mysite
-$db=mysql_select_db("chat");
-?>
